3ec1f655166964280fc84aa88c0130273c7d7c7c
Ma Eikei <maeieki@gmail.com>
2014-04-24 06:28:40 +0900

    cakephp sample

--- cakephp/app/Config/core.php
+++ cakephp/app/Config/core.php
@@ -222,12 +222,12 @@
 /**
  * A random string used in security hashing methods.
  */
-	Configure::write('Security.salt', 'DYhG93b0qyJfIxfs2guVoUubWwvniR2G0FgaC9mi');
+	Configure::write('Security.salt', 'ZlPjiTxOm0bCSWmTCtomySgD0vfVqmO6BeKPnGCI');
 
 /**
  * A random numeric string (digits only) used to encrypt/decrypt strings.
  */
-	Configure::write('Security.cipherSeed', '76859309657453542496749683645');
+	Configure::write('Security.cipherSeed', '56581526813776344689589180057');
 
 /**
  * Apply timestamps with the last modified time to static assets (js, css, images).
